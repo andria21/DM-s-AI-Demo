3890dcad7427979a2fd0965445fb03073e88c995
andria21 <andriamarqarovi33@gmail.com>
2025-07-01 02:26:19 +0400

    removed gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning="true">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <GoogleTagManager />
+        {/* <GoogleTagManager /> */}
         {children}
         <ChatWidget />
       </body>
--- src/components/gtm/gtm.jsx
+++ src/components/gtm/gtm.jsx
@@ -1,56 +1,56 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import Script from "next/script";
+// import { useEffect } from "react";
+// import Script from "next/script";
 
-const GoogleTagManager = () => {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      "gtm.start": new Date().getTime(),
-      event: "gtm.js",
-    });
+// const GoogleTagManager = () => {
+//   useEffect(() => {
+//     window.dataLayer = window.dataLayer || [];
+//     window.dataLayer.push({
+//       "gtm.start": new Date().getTime(),
+//       event: "gtm.js",
+//     });
 
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtm.js?id=GTM-NJZXZHCW";
-    document.head.appendChild(script);
+//     const script = document.createElement("script");
+//     script.async = true;
+//     script.src = "https://www.googletagmanager.com/gtm.js?id=GTM-NJZXZHCW";
+//     document.head.appendChild(script);
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
+//     return () => {
+//       document.head.removeChild(script);
+//     };
+//   }, []);
 
-  return (
-    <>
-      <Script id="gtm-script" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NJZXZHCW');`}
-      </Script>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-SNLLTHERBY"
-      ></script>
-      <script>
-        {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+//   return (
+//     <>
+//       <Script id="gtm-script" strategy="afterInteractive">
+//         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//         })(window,document,'script','dataLayer','GTM-NJZXZHCW');`}
+//       </Script>
+//       <script
+//         async
+//         src="https://www.googletagmanager.com/gtag/js?id=G-SNLLTHERBY"
+//       ></script>
+//       <script>
+//         {`window.dataLayer = window.dataLayer || [];
+//           function gtag(){dataLayer.push(arguments);}
+//           gtag('js', new Date());
 
-          gtag('config', 'G-SNLLTHERBY');`}
-      </script>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NJZXZHCW"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
-    </>
-  );
-};
+//           gtag('config', 'G-SNLLTHERBY');`}
+//       </script>
+//       <noscript>
+//         <iframe
+//           src="https://www.googletagmanager.com/ns.html?id=GTM-NJZXZHCW"
+//           height="0"
+//           width="0"
+//           style={{ display: "none", visibility: "hidden" }}
+//         ></iframe>
+//       </noscript>
+//     </>
+//   );
+// };
 
-export default GoogleTagManager;
+// export default GoogleTagManager;
