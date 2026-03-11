3dfea58ab1cb891079ff9be5db97db6d41c3dec6
andria21 <andriamarqarovi33@gmail.com>
2026-03-12 00:45:50 +0400

    ambassadori new

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning="true">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
         {/* <GoogleTagManager /> */}
         {children}
         <ChatWidget />
--- src/components/dm-overlay.jsx
+++ src/components/dm-overlay.jsx
@@ -7,7 +7,7 @@ export default function ChatWidget() {
     const script = document.createElement("script");
     script.innerHTML = `
       window.VG_CONFIG = {
-        ID: "hyj4d63gebkps3xn",
+        ID: "lbvhi8XJKf9NhgR1tNSQ",
         region: 'eu',
         render: 'bottom-right',
 
